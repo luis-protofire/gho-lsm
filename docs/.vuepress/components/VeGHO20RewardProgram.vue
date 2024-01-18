580c26e45218a9cf22d57f47701ed75038df5fd1
Artur Rodrigues de Moura <armoura96@gmail.com>
2024-01-18 13:24:44 -0300

    rewards program basic ui

---
+++ docs/.vuepress/components/VeGHO20RewardProgram.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { createProviderComponent } from '../providers/createProviderComponent';
+import { provideNetwork } from '../providers/network';
+import { provideVeSystem } from '../providers/veSystem';
+import { provideTabs, Tab } from '../providers/tabs';
+import TabsComponent from './vegho20/RewardProgram/TabsComponent.vue';
+
+const NetworkProvider = createProviderComponent(() => provideNetwork());
+const VeSystemProvider = createProviderComponent(() => provideVeSystem());
+const TabsProvider = createProviderComponent(() =>
+  provideTabs(Tab.ADMIN_POOLS)
+);
+</script>
+
+<template>
+  <NetworkProvider>
+    <VeSystemProvider>
+      <TabsProvider>
+        <TabsComponent />
+      </TabsProvider>
+    </VeSystemProvider>
+  </NetworkProvider>
+</template>
--- docs/vegho20/for-crypto-projects/step-4.md
+++ docs/vegho20/for-crypto-projects/step-4.md
@@ -3,3 +3,5 @@ title: Add your own reward programs
 ---
 
 # Add your own reward programs
+
+<VeGHO20RewardProgram />
